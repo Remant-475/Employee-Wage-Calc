41c78eeb5059e305a162be372f84d0407d6824cb
remant <remantmahato9798@gmail.com>
2022-02-23 20:36:23 +0530

    [Remant]sorting daily wage

--- empwagecalc.sh
+++ empwagecalc.sh
@@ -1,6 +1,7 @@
 #!/bin/Bash -x
 
 echo "Welcome to Employee Wage Computation Program"
+declare -a dailyWage
 isPartTimePresent=2
 isFullTimePresent=1
 isAbsent=0
@@ -9,6 +10,7 @@ workingDays=20
 maxHrsInMonth=100
 monthSalary=0
 totalWorkingHr=0
+
 function getWorkingHours() {
 	case $empCheck in
 		$isFullTimePresent)
@@ -28,7 +30,9 @@ for (( i=0; i<$workingDays && $totalWorkingHr<$maxHrsInMonth; i++ ))
 do
 	empCheck=$((RANDOM%3))
 	workingHr="$( getWorkingHours $empCheck )"
+	dailyWage[i]=$((workingHr*wagePerHour))
 	totalWorkingHr=$((totalWorkingHr+workingHr))
 done
+echo "Employee Daily wage : ${dailyWage[@]}"
 monthSalary=$((wagePerHour*totalWorkingHr))
-echo "Employee monthly wage : $monthSalary
+echo "Employee monthly wage : $monthSalary"
