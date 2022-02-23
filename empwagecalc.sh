a5dfbef0d2e82e3f50426b28d17ef249ff2062b8
remant <remantmahato9798@gmail.com>
2022-02-23 19:33:22 +0530

    [Remant] employee present or not

--- empwagecalc.sh
+++ empwagecalc.sh
@@ -1,3 +1,12 @@
 #!/bin/Bash -x
 
-echo hello
+echo "Welcome to Employee Wage Computation Program"
+present=1
+absent=0
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq $present ]
+then
+	echo "Employee present"
+else
+	echo "Employee absent"
+fi
